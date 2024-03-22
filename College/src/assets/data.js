2f9a25c07a29bea1ec43e24ebeac7d8ecc521381
isaacmugwimi <mugwimiisaac230@gmail.com>
2024-03-22 07:49:03 +0300

    added  images

--- College/src/assets/data.js
+++ College/src/assets/data.js
@@ -2,5 +2,6 @@ import gallery1 from "./gallery-1.png"
 import gallery2 from "./gallery-2.png"
 import gallery3 from "./gallery-3.png"
 import gallery4 from "./gallery-4.png"
+//images
 
 export const gallery = [gallery1, gallery2, gallery3, gallery4]
